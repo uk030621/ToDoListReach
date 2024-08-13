78c1b65b7479294213d3cde7e6724e788322e7c0
uk030621 <123797901+uk030621@users.noreply.github.com>
2024-08-13 22:37:19 +0100

    Changed back to using local server

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -9,7 +9,7 @@ function ToDoList() {
 
 
     useEffect(() => {
-        fetch(API_BASE_URL_Vercel)
+        fetch(API_BASE_URL_Local)
             .then((response) => response.json())
             .then((data) => setTasks(data));
     }, []);
@@ -20,7 +20,7 @@ function ToDoList() {
 
     function addTask() {
         if (newTask.trim() !== "") {
-            fetch(API_BASE_URL_Vercel, {
+            fetch(API_BASE_URL_Local, {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ text: newTask }),
@@ -34,7 +34,7 @@ function ToDoList() {
     }
 
     function deleteTask(id) {
-        fetch(`${API_BASE_URL_Vercel}/${id}`, {
+        fetch(`${API_BASE_URL_Local}/${id}`, {
             method: "DELETE",
         }).then(() => {
             setTasks((prevTasks) => prevTasks.filter((task) => task._id !== id));
@@ -42,7 +42,7 @@ function ToDoList() {
     }
 
     function moveTaskUp(id) {
-        fetch(`${API_BASE_URL_Vercel}/${id}/move-up`, {
+        fetch(`${API_BASE_URL_Local}/${id}/move-up`, {
             method: "PUT",
         }).then(() => {
             setTasks((prevTasks) => {
@@ -58,7 +58,7 @@ function ToDoList() {
     }
 
     function moveTaskDown(id) {
-        fetch(`${API_BASE_URL_Vercel}/${id}/move-down`, {
+        fetch(`${API_BASE_URL_Local}/${id}/move-down`, {
             method: "PUT",
         }).then(() => {
             setTasks((prevTasks) => {
